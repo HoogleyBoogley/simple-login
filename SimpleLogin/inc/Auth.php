21e1c4492d5e10a3447d7b320d69ffd7d70281bc
Danny Tran <hoogleyb@gmail.com>
2017-12-19 15:56:39 +0000

    Added code to create function in Auth.php

--- SimpleLogin/inc/Auth.php
+++ SimpleLogin/inc/Auth.php
@@ -12,10 +12,54 @@ class Auth extends DB
         print_r("<script>console.log('[Auth.php] Class Auth was loaded');</script>");
     }
     
+    // Set errors
+    public function setError($message)
+    {
+        
+    }
+    
+    // Show
+    public function showError()
+    {
+        if (isset($_SESSION['hb_simple_login_error'])) {
+            return true;
+            print_r($_SESSION['hb_simple_login_error']);
+            $_SESSION['hb_simple_login_error'] = null;
+        } else {
+            return false;
+        }
+    }
+    
     // Create user
     public function create($email, $password)
     {
-        DB::query("SELECT * FROM hb_simpleLogin");
+        $email = DB::conn()->real_escape_string($email);
+        $password = DB::conn()->real_escape_string($password);
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $time = date("Y-m-d H:i:s");
+        
+        $q = DB::query("SELECT * FROM hb_simpleLogin WHERE email='". $email ."'");
+        $num = DB::conn()->num_rows($q);
+        
+        if ($num == 0) {
+            
+            DB::query("INSERT INTO hb_simpleLogin (
+                `email`, 
+                `password`, 
+                `created_at`, 
+                `update_at`) VALUES (
+                    '". $email ."', 
+                    '". $password ."',
+                    '". $time ."',
+                    '". $time ."'
+                )"
+            );
+            
+            return true;
+        } else {
+            return false;
+        }
+        
     }
     
     // Create new session
